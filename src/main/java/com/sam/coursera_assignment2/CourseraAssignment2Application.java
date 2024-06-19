57a516100b8f0787b2454c40d8b56f1be6dbb93d
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-06-19 12:49:47 -0400

    coursera module2 assignment

---
+++ src/main/java/com/sam/coursera_assignment2/CourseraAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.sam.coursera_assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseraAssignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseraAssignment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/coursera_assignment2/CourseraAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.coursera_assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseraAssignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
